f77096641960b99e5a43073b3445a76c8c5a4775
Saravanan Sonia <srsonia98@yahoo.com.sg>
2022-01-27 12:45:27 -0800

    Initial commit

---
+++ src/Sudoku/ProblemState/Coordinates.java
@@ -0,0 +1,27 @@
+package Sudoku.ProblemState;
+
+public class Coordinates {
+    public final int x;
+    public final int y;
+
+    public Coordinates(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX(){
+        return x;
+    }
+
+    public int getY(){
+        return y;
+    }
+
+    @Override
+    public boolean equals(Object o){
+        if (this==o)return true;
+        if (o==null || getClass() != o.getClass())return false;
+        Coordinates that = (Coordinates) o;
+        return x == that.x && y == that.y;
+    }
+}
